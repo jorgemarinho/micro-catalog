378fcdecf3009fb82b3e54d278369de0a974d241
Jorge <jorgeluizbsi@gmail.com>
2021-06-28 09:44:24 -0300

    add sincronização com category

--- src/datasources/esv7.datasource.ts
+++ src/datasources/esv7.datasource.ts
@@ -31,6 +31,9 @@ const config = {
         }
       }
     },
+    "description": {
+      "type": "text",
+    },
     "is_active": {
       "type": "boolean"
     },
--- src/servers/rabbitmq.server.ts
+++ src/servers/rabbitmq.server.ts
@@ -1,11 +1,19 @@
 import {Context} from '@loopback/context';
 import {Server} from '@loopback/core';
-import {connect, Connection} from 'amqplib';
-
+import {repository} from '@loopback/repository';
+import {Channel, connect, Connection, Replies} from 'amqplib';
+import {CategoryRepository} from '../repositories';
+import AssertQueue = Replies.AssertQueue;
+import AssertExchange = Replies.AssertExchange;
 export class RabbitmqServer extends Context implements Server {
 
   private _listening: boolean;
   conn: Connection
+  channel: Channel
+
+  constructor(@repository(CategoryRepository) private categoryRepo: CategoryRepository) {
+    super();
+  }
 
   async start(): Promise<void> {
     this.conn = await connect({
@@ -15,7 +23,54 @@ export class RabbitmqServer extends Context implements Server {
     })
 
     this._listening = true;
-    return undefined;
+    this.boot();
+  }
+
+  async boot() {
+    this.channel = await this.conn.createChannel();
+    const queue: AssertQueue = await this.channel.assertQueue('micro-catalog/sync-videos');
+    const exchange: AssertExchange = await this.channel.assertExchange('amq.topic', 'topic');
+
+    await this.channel.bindQueue(queue.queue, exchange.exchange, 'model.*.*');
+
+    this.channel.consume(queue.queue, (message) => {
+
+      if (!message) {
+        return;
+      }
+
+      const data = JSON.parse(message.content.toString());
+      const [model, event] = message.fields.routingKey.split('.').slice(1);
+
+      console.log(data);
+
+      this
+        .sync({model, event, data})
+        .then(() => this.channel.ack(message))
+        .catch(() => this.channel.reject(message, false))
+    });
+
+  }
+
+  async sync({model, event, data}: {model: string, event: string, data: any}) {
+
+    if (model === 'category') {
+      switch (event) {
+        case 'created':
+          await this.categoryRepo.create({
+            ...data,
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString()
+          });
+          break;
+        case 'updated':
+          await this.categoryRepo.updateById(data.id, data);
+          break;
+        case 'deleted':
+          await this.categoryRepo.deleteById(data.id);
+          break;
+      }
+    }
   }
 
   async stop(): Promise<void> {
